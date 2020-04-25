777bb2bac0cd42a8e76ea0a94f6815043b51d974
Allan A Martell <allanmar@umich.edu>
2020-04-25 10:16:38 -0400

    explanation of origins of PVector in python

--- 01 - Vectors/physics/vector.py
+++ 01 - Vectors/physics/vector.py
@@ -2,7 +2,7 @@
 
 # URL: https://gist.github.com/mcleonard/5351452
 
-""" 
+"""
 The MIT License (MIT)
 Copyright (c) 2015 Mat Leonard
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,11 +28,11 @@ class Vector(object):
         """ Create a vector, example: v = Vector(1,2) """
         if len(args)==0: self.values = (0,0)
         else: self.values = args
-            
+
     def norm(self):
         """ Returns the norm (length, magnitude) of the vector """
         return math.sqrt(sum( comp**2 for comp in self ))
-        
+
     def argument(self):
         """ Returns the argument of the vector, the angle clockwise from +y."""
         arg_in_rad = math.acos(Vector(0,1)*self/self.norm())
@@ -45,9 +45,9 @@ class Vector(object):
         norm = self.norm()
         normed = tuple( comp/norm for comp in self )
         return Vector(*normed)
-    
+
     def rotate(self, *args):
-        """ Rotate this vector. If passed a number, assumes this is a 
+        """ Rotate this vector. If passed a number, assumes this is a
             2D vector and rotates by the passed value in degrees.  Otherwise,
             assumes the passed value is a list acting as a matrix which rotates the vector.
         """
@@ -61,10 +61,10 @@ class Vector(object):
             if not all(len(row) == len(v) for row in matrix) or not len(matrix)==len(self):
                 raise ValueError("Rotation matrix must be square and same dimensions as vector")
             return self.matrix_mult(matrix)
-        
+
     def _rotate2D(self, theta):
         """ Rotate this vector by theta in degrees.
-            
+
             Returns a new vector.
         """
         theta = math.radians(theta)
@@ -73,29 +73,29 @@ class Vector(object):
         x, y = self.values
         x, y = dc*x - ds*y, ds*x + dc*y
         return Vector(x, y)
-        
+
     def matrix_mult(self, matrix):
         """ Multiply this vector by a matrix.  Assuming matrix is a list of lists.
-        
+
             Example:
             mat = [[1,2,3],[-1,0,1],[3,4,5]]
             Vector(1,2,3).matrix_mult(mat) ->  (14, 2, 26)
-         
+
         """
         if not all(len(row) == len(self) for row in matrix):
-            raise ValueError('Matrix must match vector dimensions') 
-        
-        # Grab a row from the matrix, make it a Vector, take the dot product, 
+            raise ValueError('Matrix must match vector dimensions')
+
+        # Grab a row from the matrix, make it a Vector, take the dot product,
         # and store it as the first component
         product = tuple(Vector(*row)*self for row in matrix)
-        
+
         return Vector(*product)
-    
+
     def inner(self, other):
         """ Returns the dot product (inner product) of self and other vector
         """
         return sum(a * b for a, b in zip(self, other))
-    
+
     def __mul__(self, other):
         """ Returns the dot product of self and other if multiplied
             by another Vector.  If multiplied by an int or float,
@@ -106,46 +106,50 @@ class Vector(object):
         elif type(other) == type(1) or type(other) == type(1.0):
             product = tuple( a * other for a in self )
             return Vector(*product)
-    
+
     def __rmul__(self, other):
         """ Called if 4*self for instance """
         return self.__mul__(other)
-            
+
     def __div__(self, other):
         if type(other) == type(1) or type(other) == type(1.0):
             divided = tuple( a / other for a in self )
             return Vector(*divided)
-    
+
     def __add__(self, other):
         """ Returns the vector addition of self and other """
         added = tuple( a + b for a, b in zip(self, other) )
         return Vector(*added)
-    
+
     def __sub__(self, other):
         """ Returns the vector difference of self and other """
         subbed = tuple( a - b for a, b in zip(self, other) )
         return Vector(*subbed)
-    
+
     def __iter__(self):
         return self.values.__iter__()
-    
+
     def __len__(self):
         return len(self.values)
-    
+
     def __getitem__(self, key):
         return self.values[key]
-        
+
     def __repr__(self):
         return str(self.values)
 
 
+# Allan Martell's expansion of Mat Leonard's vector class (2020).
+# Licensed under the same MIT lisence as the original vector class.
+# The subclass PVector here takes the name from the eponymous class
+# from processing (https://processing.org/) which is written in Java
 
 class PVector(Vector):
-    
+
     def __init__(self, *args):
         """ Create a vector, example: v = Vector(1,2) """
         if len(args)==0: self.values = (0,0)
-        else: 
+        else:
             self.values = args
             self.values = list(self.values)
 
@@ -164,7 +168,7 @@ class PVector(Vector):
             new = PVector(0,0,0)
             for i, v in enumerate(self.values):
                 new.values[i] += ((length - self.values[i])**2)
-            resizedV = tuple(math.sqrt(v) for v in new.values[i]) 
+            resizedV = tuple(math.sqrt(v) for v in new.values[i])
             return PVector(resizedV)
         else: # this might change
             return None
